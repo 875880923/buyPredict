b8b9b9ca41ce82bc9d8942bf3aeac46cae9e786a
lantian <lantian@daojia.com>
2016-12-09 18:43:32 +0800

    重复购买 用户-商品 表

---
+++ gen_X_features/__init__.py
@@ -0,0 +1,8 @@
+fromDateTrain = '20160501'
+toDateTrain = '20160831'
+
+fromDateTest = '20160901'
+toDateTest = '20161112'
+
+fromDateSubmit = '20161113'
+toDateSubmit = '20160513'
--- gen_X_features/item_repeat_buy.py
+++ gen_X_features/item_repeat_buy.py
@@ -1,2 +1,23 @@
 
-print("hello")
+from gen_X_features import fromDateTrain, toDateTrain
+
+featureName = 'repeat_buy'
+
+
+def gen(from_date=None, to_date=None, table_name=None):
+    sql_template = """
+DROP TABLE IF EXISTS {table_name};
+CREATE TABLE {table_name} AS
+SELECT * FROM (
+SELECT user_id,item_id,COUNT(*) as count FROM user_log
+WHERE time_stamp >= '{from_date}' AND time_stamp <= '{to_date}' AND action_type='2'
+GROUP BY user_id,item_id
+)purchase_count
+WHERE purchase_count.count>1;
+ALTER TABLE {table_name} ADD PRIMARY KEY  (`user_id`, `item_id`);
+CREATE INDEX count_idx ON {table_name} (`count`);
+"""
+    return sql_template.format(table_name=table_name, from_date=from_date, to_date=to_date)
+
+if __name__ == '__main__':
+    print(gen(fromDateTrain, toDateTrain, 'item_%s_train' % featureName))
