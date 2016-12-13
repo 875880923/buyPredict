c1050f4594879c4bc7bac0815a464e08e83340e6
lantian <lantian@daojia.com>
2016-12-13 20:21:19 +0800

    计算ROC

--- evaluate/cal_score.py
+++ evaluate/cal_score.py
@@ -1,12 +1,17 @@
-import pymysql as dB
 from evaluate import connect
+import numpy as np
+from sklearn import metrics
 
 
 def cal_auc():
     cur = connect.cursor()
-    cur.execute("SELECT VERSION()")
-    data = cur.fetchone()
-    print("Database version : %s " % data)
+    cur.execute("SELECT * FROM train_data limit 0,2")
+    rows = cur.fetchall()
+    data = np.array(rows, dtype='float')
+    label = data[:, 2:3]
+    prob = data[:, 3:4]
+    result = metrics.auc(label, prob, True)
+    print("%.4f" % result)
 
 if __name__ == '__main__':
     cal_auc()
